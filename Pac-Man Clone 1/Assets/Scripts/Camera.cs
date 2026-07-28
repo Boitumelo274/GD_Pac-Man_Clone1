a9bf3c35d8f2db260eb2f08cfaf9b85f6cd991f4
tokidokiyukiii <2658083@students.wits.ac.za>
2026-07-28 23:21:52 +0200

    Added Camera and fixed player movement

---
+++ Pac-Man Clone 1/Assets/Scripts/Camera.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+    public Transform player;
+    void Update()
+    {
+        transform.position = new Vector3(player.position.x,player.position.y,transform.position.z);
+    }
+}

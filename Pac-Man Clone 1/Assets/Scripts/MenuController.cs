d3869ee75382a7ec83051e254fe076b4d81556f0
tokidokiyukiii <2658083@students.wits.ac.za>
2026-08-03 21:07:02 +0200

    Menu anime transition

--- Pac-Man Clone 1/Assets/Scripts/MenuController.cs
+++ Pac-Man Clone 1/Assets/Scripts/MenuController.cs
@@ -1,14 +1,24 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEditor;
+using System.Collections;
 
 public class MenuController : MonoBehaviour
 {
     public GameObject menuPanel;
+    public GameObject transitionPanel;
     public string gameSceneName = "SampleScene";
+    private const float DELAY_TIME = 2f;
 
     public void StartGame()
     {
+        StartCoroutine(LoadSceneWithDelay());
+    }
+
+    private IEnumerator LoadSceneWithDelay()
+    {
+        transitionPanel.SetActive(true);
+        yield return new WaitForSeconds(DELAY_TIME);
         SceneManager.LoadScene(gameSceneName, LoadSceneMode.Single);
     }
 
